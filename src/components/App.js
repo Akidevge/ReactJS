24ab1a6f1f75f5c62ef6c02aeca2fa9244c3b978
Akidevge <zurajanai737@gmail.com>
2023-07-02 13:41:29 +0530

    Added Keeper app V2

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import notes from "../notes"
+function Addnotes(note)
+{
+return(
+  <Note
+  key={note.id}
+  title={note.title}
+  content={note.content}
+  />
+);
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(Addnotes)}
       <Footer />
     </div>
   );
-}
-
+ }
 export default App;
